5f345f1c669e8a34b1e7208ffe6a76169d6167fa
destiny_123 <akrati.chaturvedi_cs16@gla.ac.in>
2019-04-04 23:54:55 +0530

    added GUI file

---
+++ __init_gui__.py
@@ -0,0 +1,24 @@
+from tokenise_escape_words import TokeniseAndStopWords
+from speechtotext import speechtotext
+
+def menuDriven(choice):
+    print("The Working Begin: ")
+    root.title('SQL Query Generator')
+    root.bind('<Return>', self.parse)
+
+    self.sentence_frame = LabelFrame(root, text="Input Question", padx=5, pady=5)
+    self.sentence_frame.pack(fill="both", expand="yes", padx=10, pady=5)
+
+    
+
+
+
+
+
+if(__name__=='__main__'):
+    root = Tk()
+    menuDriven(root)
+    root.resizable(width=FALSE, height=FALSE)
+    root.mainloop()
+    
+
