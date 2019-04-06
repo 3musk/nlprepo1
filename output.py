a07493b7635b11b6b1096d31198f1d1af8baa094
manika2mani <manika2mani@gmail.com>
2019-04-06 15:40:45 +0530

    updated output format

--- output.py
+++ output.py
@@ -10,4 +10,29 @@ class Final_Output:
     def read_query(self):
         print(*query_op)
         #print(" ".join(query_op))
+    def output_query(self,col_of_select,table_name,col_of_where,value_of_where):
+    
+        self.col_of_select=col_of_select
+        self.table_name=table_name
+        self.col_of_where=col_of_where
+        self.value_of_where=value_of_where
+
+        print("SELECT",end=" ")
+        if(self.col_of_select is None):
+            print("*",end=" ")
+        else:
+            i=0
+            for i in range(len(self.col_of_select)-1):
+                print(self.col_of_select[i],end=",")
+            print(self.col_of_select[i+1],end=" ")
+        print("FROM",end=" ")
+        print(*self.table_name,end=" ")
+        if(self.value_of_where is not None):
+            print("WHERE",end= " ")
+            print(*self.col_of_where,end=" = ")
+            print("\"",self.value_of_where,"\"")
+
+
+
+
     
